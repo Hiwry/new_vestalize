53edb6b522d5b79d53b04577d2f4eb6605ca3712
Hiwry <hiwry-keveny2013@hotmail.com>
2026-02-13 13:54:42 -0300

    fix: Corrigido redirecionamento do Kanban no painel de producao

--- resources/views/production/index.blade.php
+++ resources/views/production/index.blade.php
@@ -80,7 +80,7 @@
                         <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path></svg>
                         Novo Pedido
                     </a>
-                    <a href="{{ route('kanban.index') }}" 
+                    <a href="{{ route('production.kanban') }}" 
                        class="inline-flex items-center px-6 py-3 bg-gray-100 dark:bg-white/5 text-gray-700 dark:text-gray-200 rounded-xl font-bold hover:bg-gray-200 dark:hover:bg-white/10 transition border border-gray-200 dark:border-white/10">
                         <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2"></path></svg>
                         Ver em Kanban
