3a19953f57eb78a6b5d3f70933a1c7c7cacc757d
Hiwry <hiwry-keveny2013@hotmail.com>
2026-03-16 09:28:08 -0300

    Fix: correct detalhe field serialization as array in order wizard and update assets

--- resources/views/orders/wizard/sewing.blade.php
+++ resources/views/orders/wizard/sewing.blade.php
@@ -3171,11 +3171,13 @@ html.dark.avento-theme #sewing-wizard-modal *::after {
         if (sizeContainer) {
             sizeContainer.innerHTML = '';
             const detailIds = getWizardDetalhes().map(detail => detail.id);
-            const detalheInput = document.createElement('input');
-            detalheInput.type = 'hidden';
-            detalheInput.name = 'detalhe';
-            detalheInput.value = detailIds.join(',');
-            sizeContainer.appendChild(detalheInput);
+            detailIds.forEach(id => {
+                const input = document.createElement('input');
+                input.type = 'hidden';
+                input.name = 'detalhe[]';
+                input.value = id;
+                sizeContainer.appendChild(input);
+            });
 
             if (wizardData.individual_detail_colors) {
                 Object.entries(wizardData.detail_colors || {}).forEach(([detailId, colorId]) => {
